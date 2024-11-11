0fde7f64e8cb85927239c5863a874c12184a6b99
denny <dennyl@synopsys.com>
2024-11-11 22:36:12 +0800

    Display keypresses
    Display user type in keyboard also shows ASCII code, identify if it is an control character(Enter, Page Up,Delete, Ctrl-A...) by "iscntrl" function.

--- denny_editor.c
+++ denny_editor.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -19,7 +21,12 @@ void enableRawMode() {
 int main() {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
