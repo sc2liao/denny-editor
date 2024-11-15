8e126e24fc6ee9e59477362ec29692dd701f1331
denny <dennyl@synopsys.com>
2024-11-15 17:10:45 +0800

    Error handling
    Add a "die()" function to handle the error.
    "perror()" comes from <stdio.h>, and "exit()" comes from <stdlib.h>.
    "perror()" function will looks at golbal 'errno' which is set by C
    library function, then prints a descriptive errpr message for it.
    Also will give information about which function caused the error.
    
    After that use 'exit()' function to exit the program with any non-zero
    value.
    
    Some system (ex:Cygwin) read() function times out it returns '-1' with
    errno 'EAGAIN' ,so we exclude this errno, and not treat as an error.
    
    Note: we can make "tcgetattr()" fail by give program a text file or a pipe as the standard input instead of your terminal.
    ex:"./d_editor < denny_editor.c" , "echo test | ./d_editor"
    this will cause :"Inappropriate ioctl for device."

--- denny_editor.c
+++ denny_editor.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -6,10 +7,19 @@
 
 struct termios orig_termios;
 
-void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    die("tcgetattr");
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
@@ -20,14 +30,16 @@ void enableRawMode() {
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    die("tcsetattr");
 }
 
 int main() {
   enableRawMode();
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+      die("read");
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
